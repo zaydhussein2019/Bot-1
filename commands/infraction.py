6b522d5bc3787adeb90dd847763617ea93c119c0
asicalug <isaac.poissinger@gmail.com>
2024-11-01 21:49:08 +0100

    gn <3

--- commands/infraction.py
+++ commands/infraction.py
@@ -1,14 +1,24 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, pages
 from discord.commands import Option
 from discord.commands import SlashCommandGroup
+from discord.ext.pages import Paginator, Page
+import asyncio
+
+warns = []
+mods = []
 
 class Infractions(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
         print(f"{self.__class__.__name__} loaded")
 
-    @commands.command(
+    def get_pages(self):
+        return self.pages
+
+    infractions = SlashCommandGroup(name="infractions", description="Various ticket Commands")
+
+    @infractions.command(
         name="warn",
         description="Warn a member"
     )
@@ -34,6 +44,8 @@ class Infractions(commands.Cog):
         if reason != None:
             self.bot.settings.set(f'Infractions.{member.name}.Warns.{self.bot.settings.get(f'Infractions.{member.name}.Warns.Count')}.Reason', reason)
 
+        self.bot.settings.set(f'Infractions.{member.name}.Warns.{self.bot.settings.get(f'Infractions.{member.name}.Warns.Count')}.Mod', ctx.user.mention)
+
         if self.bot.settings.get(f'Infractions.{member.name}.Count') == None:
             self.bot.settings.set(f'Infractions.{member.name}.Count', 1)
         else:
@@ -54,7 +66,7 @@ class Infractions(commands.Cog):
         )
         embed.add_field(
             name="Infractions Count:",
-            value=self.bot.settings.get(f'Infractions.{member.name}.Count')
+            value=f"{self.bot.settings.get(f'Infractions.{member.name}.Count')}/18"
         )
         embed.set_footer(
             text="Something missing ? report to @asicalug.",
@@ -62,6 +74,62 @@ class Infractions(commands.Cog):
         )
         await ctx.interaction.edit_original_response(embed=embed)
 
+    @infractions.command(
+        name="case",
+        description="Case of a member"
+    )
+    @commands.has_permissions(moderate_members=True)
+    async def infractions_case(
+        self,
+        ctx: discord.ApplicationContext,
+        member: Option(discord.Member, description="The member's case you want to check", required=False), #type: ignore
+    ):
+        if member == None:
+            member = ctx.user
+
+        wAmount = self.bot.settings.get(f"Infractions.{member.name}.Warns.Count")
+
+        if self.bot.settings.get(f"Infractions.{member.name}.Warns.Count") != None:
+            for i in range(wAmount+1):
+                if i == 0:
+                    pass
+                else:
+                    warns.append(self.bot.settings.get(f'Infractions.{member.name}.Warns.{i}.Reason'))
+                    mods.append(self.bot.settings.get(f'Infractions.{member.name}.Warns.{str(i)}.Mod'))
+
+        joinedWarns = "\n".join(warns)
+        joinedMods = "\n".join(mods)
+        
+        self.pages = [
+            Page(
+                embeds=[
+                    discord.Embed(title=f"{member.name}'s Infractions Status", description=f"{self.bot.settings.get(f'Infractions.{member.name}.Count')}/18", color=discord.Color.from_rgb(r=0, g=146, b=253))
+                ]
+            ),
+            Page(
+                embeds=[
+                    discord.Embed(title=f"{member.name}'s Warns", description=f"", color=discord.Color.yellow()).set_thumbnail(url=member.avatar.url)
+                    .set_image(url="https://r2.e-z.host/17a2b375-7193-4f28-94c4-be10a3e7c1b4/jwy79kko.png")
+                    .add_field(name="Reasons", value=joinedWarns)
+                    .add_field(name="Mods", value=f'{joinedMods}')
+                    .set_footer(text=f"Designerlc", icon_url="https://r2.e-z.host/17a2b375-7193-4f28-94c4-be10a3e7c1b4/d5zm1xpi.webp"),
+                ],
+            ),
+            Page(
+                embeds=[
+                    discord.Embed(title=f"{member.name}'s Warns", description=f"W.I.P", color=discord.Color.yellow())
+                ]
+            )
+        ]
+        paginator = pages.Paginator(pages=self.get_pages())
+
+        await paginator.respond(ctx.interaction, ephemeral=True)
+        
+        warns.clear()
+        mods.clear()
+
+        
+        
         
 
 def setup(bot: commands.Bot):
--- listeners/on_ready.py
+++ listeners/on_ready.py
@@ -29,22 +29,6 @@ class Ready(commands.Cog):
         self.bot.add_view(uSure(bot=self.bot))
         print("Loaded views!")
 
-        
-    async def on_member_join(self, member):
-        print("hi")
-        channel = self.bot.get_channel(self.bot.settings.get('Systems.Welcoming.Channel'))
-        if channel is not None:
-            embed = discord.Embed(
-                title=f"Welcome {member.name}!",
-                description=f"Welcome to @designerlc, {member.mention}",
-                color=discord.Color.blurple()
-            )
-            embed.set_thumbnail(url=member.avatar.url)
-            embed.set_footer(text="Need help? Open a ticket!", icon_url="https://r2.e-z.host/17a2b375-7193-4f28-94c4-be10a3e7c1b4/d5zm1xpi.webp")
-            await channel.send(f'Welcome {member.mention}.')
-        if channel is None:
-            print("Welcome Channel has NOT been set. I will not be able to welcome new members. Please set it by using /settings welcoming.")
-
 
 
 
